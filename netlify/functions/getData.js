c320f27e3600e8374c7793e92633f815f0a9c480
Terence <githubbrah@gmail.com>
2025-12-11 14:07:21 +0800

    Update getData.js

--- netlify/functions/getData.js
+++ netlify/functions/getData.js
@@ -1,9 +1,9 @@
-import fetch from "node-fetch";
+Iimport fetch from "node-fetch";
 
 export async function handler() {
   try {
     const res = await fetch("https://api.example.com/data", {
-      headers: { "Authorization": `Bearer ${process.env.API_KEY}` }
+      headers: { "Authorization": `Bearer ${process.env.GEMINI_API_KEY}` }
     });
     const data = await res.json();
     return { statusCode: 200, body: JSON.stringify(data) };
